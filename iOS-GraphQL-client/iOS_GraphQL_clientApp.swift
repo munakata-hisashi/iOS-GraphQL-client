c973e0997995475384d758897f653c1db8b8e2c7
宗像恒 <hisashi@Mac-mini.local>
2021-07-19 21:22:59 +0900

    Initial commit

---
+++ iOS-GraphQL-client/iOS_GraphQL_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GraphQL_clientApp.swift
+//  iOS-GraphQL-client
+//
+//  Created by 宗像恒 on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GraphQL_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-GraphQL-clientTests/iOS_GraphQL_clientTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_GraphQL_clientTests.swift
+//  iOS-GraphQL-clientTests
+//
+//  Created by 宗像恒 on 2021/07/19.
+//
+
+import XCTest
+@testable import iOS_GraphQL_client
+
+class iOS_GraphQL_clientTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
